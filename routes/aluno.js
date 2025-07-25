22b0cfe2cec504964215003242231ee539ddf2c5
Ricardo Andrade <ricardoandradecc0@gmail.com>
2025-07-25 19:04:26 -0300

    fix: json response to jade in aluno and blog routes

--- routes/aluno.js
+++ routes/aluno.js
@@ -4,34 +4,8 @@ const AlunoService  = require('../services/aluno.service');
 
 // lista todos
 router.get("/", (req,res) => {
-  res.json(AlunoService.listar());
-});
-
-// busca um
-router.get("/:id", (req,res) => {
-  const a = AlunoService.recuperar(req.params.id);
-  if (!a) return res.status(404).send("Aluno não encontrado");
-  res.json(a);
-});
-
-// cria
-router.post("/", (req,res) => {
-  const novo = AlunoService.criar(req.body);
-  res.status(201).json(novo);
-});
-
-// atualiza
-router.put("/:id", (req,res) => {
-  const atualizado = AlunoService.atualizar(req.params.id, req.body);
-  if (!atualizado) return res.status(404).send("Aluno não encontrado");
-  res.json(atualizado);
-});
-
-// exclui
-router.post("/:id/apagar", (req,res) => {
-  if (!AlunoService.apagar(req.params.id)) 
-    return res.status(404).send("Aluno não encontrado");
-  res.status(204).end();
+  const alunos = AlunoService.listar();
+  res.render("aluno/index", {title: "Alunos", alunos});
 });
 
 module.exports = router;
--- routes/blog.js
+++ routes/blog.js
@@ -4,34 +4,8 @@ const BlogService = require('../services/blog.service');
 
 // lista todos
 router.get('/', (req,res) => {
-  res.json(BlogService.listar());
-});
-
-// busca um
-router.get('/:id', (req,res) => {
-  const b = BlogService.recuperar(req.params.id);
-  if (!b) return res.status(404).send('Post não encontrado');
-  res.json(b);
-});
-
-// cria
-router.post('/', (req,res) => {
-  const novo = BlogService.criar(req.body);
-  res.status(201).json(novo);
-});
-
-// atualiza
-router.put('/:id', (req,res) => {
-  const atualizado = BlogService.atualizar(req.params.id, req.body);
-  if (!atualizado) return res.status(404).send('Post não encontrado');
-  res.json(atualizado);
-});
-
-// exclui
-router.delete('/:id', (req,res) => {
-  if (!BlogService.apagar(req.params.id))
-    return res.status(404).send('Post não encontrado');
-  res.status(204).end();
+  const blogs = BlogService.listar();
+  res.render('blog/index', {title: 'Blog', blogs});
 });
 
 module.exports = router;
